30673e378b7771b1fcf0afef02ba4471cef56111
Cezary Gowiński <ttgovin@gmail.com>
2020-07-23 22:16:21 +0200

    poprawnie dodano pliki

---
+++ RabbitReceive/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace RabbitReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main2();
+        }
+
+        static void Przetwarzanie()
+        {
+            Console.WriteLine("Przetwarzanie...");
+        }
+
+        static void Main1()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("task_queue", true, false, false, null);
+
+                    channel.BasicQos(0, 1, false);
+
+                    Console.WriteLine(" [*] Waiting for messages.");
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] body = ea.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("[x] received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("[x] Done");
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    };
+
+                    channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        static void Main2()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "rpc_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(0, 1, false);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+                Console.WriteLine(" [x] Awaiting RPC requests");
+
+                consumer.Received += (model, ea) =>
+                {
+                    string response = null;
+
+                    byte[] body = ea.Body.ToArray();
+                    IBasicProperties props = ea.BasicProperties;
+                    IBasicProperties replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        string message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine(" [.] fib({0})", message);
+                        response = fib(n).ToString();
+                        Console.WriteLine("sent back");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    }
+                };
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return fib(n - 1) + fib(n - 2);
+        }
+
+    }
+}
---
+++ RabbitSend/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main2(args);
+
+            Console.ReadLine();
+        }
+
+
+        static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? args[0] : "1");
+        }
+
+        static void Main1(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            {
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+
+                    string message = GetMessage(args);
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    IBasicProperties properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit");
+            Console.ReadLine();
+        }
+
+        public static void Main2(string[] args)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting fib({0})", GetMessage(args));
+            var response = rpcClient.Call(GetMessage(args));
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+        }
+
+    }
+}

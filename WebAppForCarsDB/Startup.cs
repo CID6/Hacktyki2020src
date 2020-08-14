83f4f65bd51ec3e1cc8cb8cde2b512aaf7354178
Cezary Gowiński <ttgovin@gmail.com>
2020-08-14 11:40:08 +0200

    cleanup projektów

--- WebAppForCarsDB/Startup.cs
+++ WebAppForCarsDB/Startup.cs
@@ -28,6 +28,7 @@ namespace WebAppForCarsDB
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
--- WebAppForCarsDB/wwwroot/js/movieIndex.js
+++ WebAppForCarsDB/wwwroot/js/movieIndex.js
@@ -13,12 +13,66 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const preObject = document.getElementById('object');
-
 //create reference
 //ref - root
-const dfRefObject = firebase.database().ref().child('Movies/');
+const dfRefObject = firebase.database().ref().child('movies/');
 
 //synchronize obj change
 //snap - data snapshot, returns keyname and ways to iterate children
-dfRefObject.on('value', snap => console.log(snap.val()));
+//dfRefObject.once('value', snap => parseSnap(snap.val()));
+dfRefObject.on("child_added", snap => parseNewRow(snap.val(), snap.key));
+dfRefObject.on("child_changed", snap => parseUpdatedRow(snap.val(), snap.key));
+dfRefObject.on("child_removed", snap => deleteRow(snap.key));
+
+
+
+function parseNewRow(snapValue, snapKey) {
+    var child = snapValue;
+    //console.log(snapValue);
+    //console.log(snapKey);
+    createRow(child["title"], child["release_date"], child["genre"], child["price"], snapKey);
+}
+
+function parseUpdatedRow(snapValue, snapKey) {
+    var child = snapValue;
+    console.log(snapValue);
+    console.log(snapKey);
+    updateRow(child["title"], child["release_date"], child["genre"], child["price"], snapKey);
+}
+
+function createRow(title, releasedate, genre, price, movieId) {
+    var table = document.getElementById("tablebody");
+    var row = table.insertRow(0);
+    row.id = movieId;
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML = title;
+    cell2.innerHTML = releasedate;
+    cell3.innerHTML = genre;
+    cell4.innerHTML = price;
+}
+
+function updateRow(title, releasedate, genre, price, movieId) {
+    const rowChanged = document.getElementById(movieId);
+    rowChanged.deleteCell(3);
+    rowChanged.deleteCell(2);
+    rowChanged.deleteCell(1);
+    rowChanged.deleteCell(0);
+    var cell1 = rowChanged.insertCell(0);
+    var cell2 = rowChanged.insertCell(1);
+    var cell3 = rowChanged.insertCell(2);
+    var cell4 = rowChanged.insertCell(3);
+    cell1.innerHTML = title;
+    cell2.innerHTML = releasedate;
+    cell3.innerHTML = genre;
+    cell4.innerHTML = price;
+}
+
+function deleteRow(movieId) {
+    var rowToDelete = document.getElementById(movieId);
+    var rowToDeleteIndex = rowToDelete.rowIndex;
+    var parentTable = document.getElementById("mainTable");
+    parentTable.deleteRow(rowToDeleteIndex);
+}
